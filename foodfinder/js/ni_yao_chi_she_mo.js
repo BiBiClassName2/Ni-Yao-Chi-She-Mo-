079bb90df2aab22b181641496b0a9d3aab81c334
BIBIOTA <50495950+BIBIOTA@users.noreply.github.com>
2020-09-22 12:25:25 +0800

    Update ni_yao_chi_she_mo.js

--- foodfinder/js/ni_yao_chi_she_mo.js
+++ foodfinder/js/ni_yao_chi_she_mo.js
@@ -12,6 +12,7 @@ var foods = {
   food8 : {name: 'Subway 南京復興店',href: 'https://g.page/subway_70616?share'},
   food9 : {name: '丘彼特早午餐遼寧店',href: 'https://goo.gl/maps/HSdSGa2xHwFA9Ztc8'},
   food10 : {name: '慶城海南雞飯',href: 'https://goo.gl/maps/ye3ctPXYdqC2GW6Q9'},
+  food11 : {name: '飯飯堂南京復興店,href: 'https://maps.app.goo.gl/wLo65YmzNMSL158s5'},
 }
 
 var allfoods = [];
